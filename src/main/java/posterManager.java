e6dc2d527f8b0ddcb1c34668b7ee2540dd2f24c0
dmitry <whtperson@gmail.com>
2023-06-04 10:21:29 +0300

    Initial commit

---
+++ src/main/java/posterManager.java
@@ -0,0 +1,39 @@
+public class posterManager {
+    private String[] films = new String[0];
+    private int limit;
+
+    public posterManager() {
+        this.limit = 5;
+    }
+
+    public posterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addNewPoster(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll() {
+        return films;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (films.length < limit) {
+            resultLength = films.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+}
---
+++ src/test/java/posterManagerTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class posterManagerTest {
+    @Test
+    public void AllFilm() {
+        posterManager post = new posterManager();
+        post.addNewPoster("Бладшот");
+        post.addNewPoster("Вперёд");
+        post.addNewPoster("Отель Белград");
+        post.addNewPoster("Джентельмены");
+        post.addNewPoster("Человек-невидимка");
+        post.addNewPoster("Тролли.Мировой тур");
+        post.addNewPoster("Номер один");
+        String[] expected = {"Бладшот", "Вперёд", "Отель Белград", "Джентельмены","Человек-невидимка" , "Тролли.Мировой тур", "Номер один"};
+        String[] actual = post.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void LastAddFilms() {
+        posterManager post = new posterManager();
+        post.addNewPoster("Бладшот");
+        post.addNewPoster("Вперёд");
+        post.addNewPoster("Отель Белград");
+        post.addNewPoster("Джентельмены");
+        post.addNewPoster("Человек-невидимка");
+        post.addNewPoster("Тролли.Мировой тур");
+        post.addNewPoster("Номер один");
+        String[] expected = {"Номер один", "Тролли.Мировой тур", "Человек-невидимка", "Джентельмены", "Отель Белград"};
+        String[] actual = post.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void LimitFilm() {
+        posterManager post = new posterManager();
+        post.addNewPoster("Бладшот");
+        post.addNewPoster("Вперёд");
+        post.addNewPoster("Отель Белград");
+        post.addNewPoster("Джентельмены");
+        post.addNewPoster("Человек-невидимка");
+        post.addNewPoster("Тролли.Мировой тур");
+        post.addNewPoster("Номер один");
+        String[] expected = {"Номер один", "Тролли.Мировой тур", "Человек-невидимка", "Джентельмены", "Отель Белград"};
+        String[] actual = post.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void MinLimit() {
+        posterManager post = new posterManager();
+
+        post.addNewPoster("Человек-невидимка");
+        post.addNewPoster("Тролли.Мировой тур");
+        post.addNewPoster("Номер один");
+        String[] expected = {"Номер один", "Тролли.Мировой тур", "Человек-невидимка"};
+        String[] actual = post.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
